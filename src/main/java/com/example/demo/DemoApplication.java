25aa6535feea2aa8395e12e9bdfadf0daf048dc8
Ramdhani Michandari <ramdhani.michandari@privyid.tech>
2025-01-14 13:42:01 +0700

    setup .env config

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,27 @@
 package com.example.demo;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.io.File;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	@Autowired
+	private Environment env;
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+
+		File envFile = new File(".env");
+		if (!envFile.exists()) {
+			System.err.println(".env File must be exists");
+			System.exit(1);
+		}
+
+		Dotenv dotenv = Dotenv.load();
 
 		try {
 			Class.forName("org.postgresql.Driver");
@@ -15,6 +29,14 @@ public class DemoApplication {
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
+
+		System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+		System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
+		System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+		System.setProperty("DB_USER", dotenv.get("DB_USER"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
