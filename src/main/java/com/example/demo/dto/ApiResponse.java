310f82b5da13e986887d323712b8470c163c8d02
Ramdhani Michandari <ramdhani.michandari@privyid.tech>
2025-01-23 11:24:43 +0700

    set static message

--- src/main/java/com/example/demo/dto/ApiResponse.java
+++ src/main/java/com/example/demo/dto/ApiResponse.java
@@ -9,6 +9,8 @@ public class ApiResponse<T> {
     private String message;
     private int code;
     private T data;
+    public static final String success = "Success Retrieve Data";
+    public static final String userNotFound = "User Not Found";
 
     // Constructor
     public ApiResponse(String message, int code, T data) {
