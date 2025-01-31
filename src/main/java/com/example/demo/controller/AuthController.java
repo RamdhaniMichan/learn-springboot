d7c8ba808bdda89b7d1285b84df23b08b86a2d59
Ramdhani Michandari <ramdhani.michandari@privyid.tech>
2025-01-31 13:14:52 +0700

    validate access role in sample path hello

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,15 +1,19 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.ApiResponse;
+import com.example.demo.entity.Role;
 import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
 import com.example.demo.utility.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -24,7 +28,11 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestParam String username, @RequestParam String password) {
         User user = userService.login(username, password);
-        String token = jwtUtil.generateToken(user.getUsername());
+
+        String roles = user.getUserRoles().stream()
+                .map(userRole -> userRole.getRole().getCode()).collect(Collectors.joining(","));
+
+        String token = jwtUtil.generateToken(user.getUsername(), roles);
 
         ApiResponse<?> response = new ApiResponse<>(
                 "Success Login",
@@ -36,6 +44,7 @@ public class AuthController {
     }
 
     @PostMapping("/hello")
+    @PreAuthorize("hasAuthority('user')")
     public ResponseEntity<String> hello(@RequestParam String name) {
         return ResponseEntity.ok("Hello " + name);
     }
