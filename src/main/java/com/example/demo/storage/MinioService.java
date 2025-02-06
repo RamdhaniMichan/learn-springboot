9530e54204cd5873297ce177e40c07fdb8e2b134
Ramdhani Michandari <ramdhani.michandari@privyid.tech>
2025-02-06 17:25:34 +0700

    add presignedUrl on minio service

--- src/main/java/com/example/demo/storage/MinioService.java
+++ src/main/java/com/example/demo/storage/MinioService.java
@@ -1,9 +1,11 @@
 package com.example.demo.storage;
 
 import io.minio.GetObjectArgs;
+import io.minio.GetPresignedObjectUrlArgs;
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
 import io.minio.errors.MinioException;
+import io.minio.http.Method;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class MinioService {
@@ -38,6 +41,20 @@ public class MinioService {
         } catch (IOException | InvalidKeyException | NoSuchAlgorithmException e) {
             System.err.println("Error occurred: " + e);
             throw new Exception("Error processing file: " + e.getMessage());
+        }
+    }
+
+    public String getPresignedUrl(String objectName) throws Exception {
+        try {
+            return minioClient.getPresignedObjectUrl(
+                    GetPresignedObjectUrlArgs.builder()
+                            .method(Method.GET)
+                            .bucket(bucketName)
+                            .object(objectName)
+                            .expiry(7, TimeUnit.DAYS)
+                            .build());
+        } catch (MinioException | IOException | InvalidKeyException | NoSuchAlgorithmException e) {
+            throw new Exception("Failed to generate presigned URL", e);
         }
     }
 
